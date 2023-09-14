74e76240454fae4540fb7463255ce1d14f91f1ab
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-09-13 21:50:25 -0300

    prompt-select component

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,13 @@ import {
 } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input";
+import { PromptSelect } from "./components/prompt-select";
 
 export function App() {
+  function handlePromptSelected(template: string) {
+    console.log(template);
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -57,17 +62,7 @@ export function App() {
           <form className="space-y-5">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do Youtube</SelectItem>
-                  <SelectItem value="description">
-                    Descrição do Youtube
-                  </SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
             <div className="space-y-2">
               <Label>Modelo</Label>
