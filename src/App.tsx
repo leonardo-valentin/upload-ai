fe4f62df789aebedf0bd1636f689a7edfc69f1af
leonardo-valentin <leo.v.santosbr@gmail.com>
2023-09-26 17:42:01 -0300

    connecting to deploy of api

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export function App() {
     completion,
     isLoading,
   } = useCompletion({
-    api: "http://localhost:3333/ai/complete",
+    api: "https://upload-api-ai.onrender.com/ai/complete",
     body: {
       videoId,
       temperature,
